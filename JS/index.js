695db561f381cd4f0fd68a3229db906dad9d31d0
Franco Guerra <eugeniafilippin@hotmail.com>
2023-01-19 20:40:52 -0300

    :sparkles: Feat: add structure HTML to index and about

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,9 @@
 const menu = document.getElementById('menu');
 const listaMenu = document.getElementById('lista-menu');
+const body = document.querySelector('body');
 
 menu.addEventListener('click', () => {
     console.log('clicked');
     listaMenu.classList.toggle('show');
+    body.classList.toggle('overflow');
 })
